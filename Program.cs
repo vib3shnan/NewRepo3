4c7448db3b8e2ea7343e449d3337c3a9b8cf7cca
Vibeshnan <vibeshnan2002@gmail.com>
2023-07-18 13:42:32 +0530

    comment added

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+//adding a comment to check if the main.yml is working
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
